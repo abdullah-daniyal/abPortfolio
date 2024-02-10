8f3cdf78b078a8ea13ec41bd15045f8fa92b884c
abdullah-daniyal <124662911+abdullah-daniyal@users.noreply.github.com>
2024-02-10 16:15:00 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Skills from "@/components/main/skills";
+import Projects from "@/components/main/projects";
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <main className='h-full w-full'>
+    <div className='flex flex-col gap-20'>
+       <Hero />
+       <Skills />
+       <Encryption />
+       <Projects />
+    </div>
+    
+   </main>
+    
+    );
+}
